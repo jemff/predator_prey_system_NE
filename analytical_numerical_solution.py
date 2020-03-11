Added correct calculation of fixpoints.

analytical_numerical_solution.py
@@ -46,14 +46,13 @@ def static_eq_calc(params):
 
     phitild = phi0+phi1
     mutild = mu0 + mu1
-    N_star = phitild*cp/(cp*eps-phitild) #-(epsn*cmax*lam + cp/(phitild*eps))/(cmax*epsn-cp/phitild-mutild)
-    btild = cmax*(1+N_star/lam) - cbar
+    C_star = phitild*nu1/(eps*cp-phitild)
+    gam = nu0-cbar-cmax/lam*C_star
+    R_star = (-gam+ np.sqrt(gam**2-4*cbar*nu0))/2
+    P_star = mutild*C_star/(epsn * C_star*R_star*cmax/(R_star+nu0)-cp*C_star/(C_star+nu1))
 
-    C_star = 1/2*(-btild+np.sqrt(btild**2 + 4*cbar*cmax))
-
-    P_star = (1/cp+1/N_star)*(epsn*lam*(cbar-C_star)-mutild*N_star)
 #    print(np.array([C_star, N_star, P_star]))
-    return np.array([C_star, N_star, P_star])
+    return np.array([R_star, C_star, P_star])
 
 def opt_taup_find(y, s_prey, params):
     k = s_prey * y[1] / params['cp']
@@ -274,23 +273,18 @@ def heatmap_plotter(data, title, image_name, ext):
 
     plt.savefig(image_name+".png", bbox_inches='tight')
 
-base = 10
-its = 80
-step_size = 0.5
-step_size_phi = 0.005 #25 #0.00125
-cbar = base
-phi0_base = 0.2
-
 cmax = 2
 mu0 = 0.2
 mu1 = 0.2
-epsn = 0.7
-
 eps = 0.7
-cp = 2
-phi0 = phi0_base
-phi1 = 0.2
+epsn = 0.7
+cp = 8 #m,i
+phi0 = 0.8 #phi0_base
+phi1 = 0.8
 lam = 0.5
+
+
+base = 10 #3 #*mass_vector[0]**(-0.25) #0.01
 
 opt_prey = True
 opt_pred = True
@@ -317,7 +311,7 @@ if manual_max is True:
 eq_stat = static_eq_calc(params_ext)
 #sol_4 = optm.root(lambda y: optimal_behavior_trajectories(y, params_ext), x0 = np.array([base, base, 4/10*base]), method = 'hybr') #Apparently only three real roots.
 
-print(optimal_behavior_trajectories(np.array([6.835213942880785, 9.616519943168255, 3.731972978096975]), params_ext))
+print(optimal_behavior_trajectories(np.array([3.823177394403382, 2.244085194755178, 0.15802986175068165]), params_ext))
 
 sol_3 = optm.root(lambda y: optimal_behavior_trajectories(y, params_ext), x0 = np.array([8.85361793, 6.85670493, 6.48515033]), method = 'hybr')
 #sol_3 = optm.root(lambda y: optimal_behavior_trajectories(y, params_ext), x0 = np.array([12.24914961,  0.8,  0.98489586]), method = 'hybr')

semi_impl_eul.py
@@ -284,6 +284,18 @@ def semi_implicit_euler(t_final, y0, step_size, f, params, opt_prey = True, opt_
         flux[:, i] = fwd[3:]
     return t, solution, flux, strat
 
+def static_eq_calc(params):
+    cmax, mu0, mu1, eps, epsn, cp, phi0, phi1, cbar, lam, nu0, nu1 = params.values()
+
+    phitild = phi0+phi1
+    mutild = mu0 + mu1
+    C_star = phitild*nu1/(eps*cp-phitild)
+    gam = nu0-cbar-cmax/lam*C_star
+    R_star = (-gam+ np.sqrt(gam**2-4*cbar*nu0))/2
+    P_star = mutild*C_star/(epsn * C_star*R_star*cmax/(R_star+nu0)-cp*C_star/(C_star+nu1))
+
+#    print(np.array([C_star, N_star, P_star]))
+    return np.array([R_star, C_star, P_star])
 
 
 #base = 10
@@ -292,29 +304,29 @@ step_size = 0.5*2.5
 step_size_phi = 0.0025*2.5 #0.00125
 #phi0_base = 0.2
 
-#cmax = 2
-#mu0 = 0.2
-#mu1 = 0.2
-#eps = 0.7
-#epsn = 0.7
-#cp = 2 m,i
-#phi0 = 0.4 #phi0_base
-#phi1 = 0.2
-#lam = 0.5
+cmax = 2
+mu0 = 0.2
+mu1 = 0.2
+eps = 0.7
+epsn = 0.7
+cp = 8 #m,i
+phi0 = 0.8 #phi0_base
+phi1 = 0.8
+lam = 0.5
 
-cost_of_living, nu, growth_max, lam = parameter_calculator_mass(mass_vector)
+#cost_of_living, nu, growth_max, lam = parameter_calculator_mass(mass_vector)
 
-base = 50 #3 #*mass_vector[0]**(-0.25) #0.01
+base = 10 #3 #*mass_vector[0]**(-0.25) #0.01
 cbar = base
-phi0_base = cost_of_living[1]#/2#*5
-phi1 = cost_of_living[1]*2#/2#*5
-phi0 = phi0_base
+#phi0_base = cost_of_living[1]#/2#*5
+#phi1 = cost_of_living[1]*2#/2#*5
+#phi0 = phi0_base
 
-cmax, cp = growth_max
-mu0 = cost_of_living[0] #*2#/2#*5 #*60 #/2
-mu1 = cost_of_living[0]*2 #*2#/2#*5 #*2 #*10 #/2
-nu0 = nu[0] #nu
-nu1 = nu[1] #nu
+#cmax, cp = growth_max
+#mu0 = cost_of_living[0] #*2#/2#*5 #*60 #/2
+#mu1 = cost_of_living[0]*2 #*2#/2#*5 #*2 #*10 #/2
+nu0 = 2 #nu[0] #nu
+nu1 = 2 #nu[1] #nu
 
 eps = 0.4 #0.17
 epsn =  0.4 #0.17
@@ -425,17 +437,17 @@ if its > 0:
 
 
 print(mu0, mu1, cmax, nu0, phi0, phi1, cp, nu1)
-t_end = 20
+t_end = 100
 
 init = np.array([3.252678746139226, 2.2460419897613613, 0.7498879285084376]) #np.array([5.753812957581866, 5.490194692112937, 1.626801718856221])#
 tim, sol, flux, strat = semi_implicit_euler(t_end, init, 0.001, lambda t, y, tn, tp:
 optimal_behavior_trajectories(t, y, params_ext, taun=tn, taup=tp, seasons = False), params_ext, opt_prey=True, opt_pred=True)
 tim, sol_2, flux_2, strat_2 = semi_implicit_euler(t_end, init, 0.001, lambda t, y, tn, tp:
 optimal_behavior_trajectories(t, y, params_ext, taun=tn, taup=tp, seasons = False), params_ext, opt_prey=False, opt_pred=False)
-#tim, sol_3, flux_3, strat_3 = semi_implicit_euler(t_end, init, 0.01, lambda t, y, tn, tp:
-#optimal_behavior_trajectories(t, y, params_ext, taun=tn, taup=tp, seasons = False), params_ext, opt_prey=True, opt_pred=False)
-#tim, sol_4, flux_4, strat_4 = semi_implicit_euler(t_end, init, 0.01, lambda t, y, tn, tp:
-#optimal_behavior_trajectories(t, y, params_ext, taun=tn, taup=tp, seasons = False), params_ext, opt_prey=False, opt_pred=True)
+tim, sol_3, flux_3, strat_3 = semi_implicit_euler(t_end, init, 0.001, lambda t, y, tn, tp:
+optimal_behavior_trajectories(t, y, params_ext, taun=tn, taup=tp, seasons = False), params_ext, opt_prey=True, opt_pred=False)
+tim, sol_4, flux_4, strat_4 = semi_implicit_euler(t_end, init, 0.001, lambda t, y, tn, tp:
+optimal_behavior_trajectories(t, y, params_ext, taun=tn, taup=tp, seasons = False), params_ext, opt_prey=False, opt_pred=True)
 
 print(sol_2)
 C, N, P =  C, N, P = sol[:,-1]
@@ -506,15 +518,25 @@ plt.savefig("Indsvingning_strat.png")
 
 print(strat[0,1:])
 
-#plt.figure()
-#plt.plot(tim, sol[0,:], label = 'Resource biomass')
-#plt.plot(tim, sol_3[1,:], label = 'Dynamic prey biomass', color = 'Green')
-#plt.plot(tim, sol_3[2,:], label = 'Static predator biomass', color = 'Red')
-#plt.plot(tim, sol_4[1,:], label = 'Static prey biomass', linestyle = '-.', color = 'Green')
-#plt.plot(tim, sol_4[2,:], label = 'Dynamic predator biomass', linestyle = '-.', color = 'Red')
+plt.figure()
+plt.plot(tim, sol_3[1,:], label = 'Dynamic prey biomass', color = 'Green')
+plt.plot(tim, sol_3[2,:], label = 'Static predator biomass', color = 'Red')
 
-#plt.xlabel("Months")
-#plt.ylabel("g/m^3")
-#plt.legend(loc = 'upper left')
+plt.title("Dynamic prey, static predator")
+plt.xlabel("Months")
+plt.ylabel("g/m^3")
+plt.legend(loc = 'upper left')
 
-#plt.savefig("Indsvingning_mixed.png")
+plt.savefig("Indsvingning_dynprey.png")
+
+plt.figure()
+plt.plot(tim, sol_4[1,:], label = 'Static prey biomass', linestyle = '-.', color = 'Green')
+plt.plot(tim, sol_4[2,:], label = 'Dynamic predator biomass', linestyle = '-.', color = 'Red')
+
+plt.title("Static prey, dynamic predator")
+plt.xlabel("Months")
+plt.ylabel("g/m^3")
+plt.legend(loc = 'upper left')
+
+plt.savefig("Indsvingning_mixed.png")
+

testing_file.py
@@ -1,50 +1,55 @@
 import numpy as np
 
-mass_vector = np.array([1, 10, 1000])
+def static_eq_calc(params):
+    cmax, mu0, mu1, eps, epsn, cp, phi0, phi1, cbar, lam, nu0, nu1 = params.values()
 
-def parameter_calculator(mass_vector):
-    X0 = 10 **(-4) #kg**(0.75) m^(-3)
-    seconds_per_month = 6 #2419200
-    D = 3
-    no_specs = 2
-    no_specs = no_specs
-    a0 = 10**(-1.77) #m^3 s^(-1) kg^(-1.05)
-    V0 = 0.33 #ms^(-1) kg^pv)
-    pd = 0.2 #Unitless
-    d0 = 1.62 #m kg^(2pd)
-    pv = 0.25 #Unitless
-    Rp = 0.1  #Preferred ratio body-size
-    h0 = 0.01 #10 ** 4 #This seems reasonable since the value range is very wide.  # kg**(beta-1) m
-    r0 = 10#1.71 * 10**(-6) * seconds_per_month #kg**(1-beta) / month
-    L = 1  # kg^(1-beta)s^(-1)
-    beta = 0.75 #Metabolic scaling constant
-    K0 = 100 #kg^beta m^(-3)
-    Cbar0 = 0.01 #Kg biomass pr m^3
-    efficiency = 0.7  # Arbitrary
-    loss_rate = 4.15 * 10 ** (-2) # kg**(beta) m**(-3) #We just multiplied by 1 million because of reasons We s
+    phitild = phi0+phi1
+    mutild = mu0 + mu1
+    C_star = phitild*nu1/(eps*cp-phitild)
+    gam = nu0-cbar+(cmax/lam)*C_star
+    print(gam, gam**2, 4*cbar*nu0, np.sqrt(gam**2+4*cbar*nu0))
+    R_star = (-gam + np.sqrt(gam**2+4*cbar*nu0))/2
+    P_star = (epsn * C_star*R_star*cmax/(R_star+nu0)-mutild*C_star)/(cp*C_star/(C_star+nu1))
 
-    alpha = a0*mass_vector**(pv+2*pd*(D-1))
-    search_rate = np.pi * V0 * d0 ** 2 \
-                  * (mass_vector[1:]) ** (2/(3)) \
-                  * (mass_vector[0:2] ** (2/3))*seconds_per_month
-
-    inner_term = Rp*1/mass_vector[0:2]  # Simplified
-    second_term = (1 + (np.log10(inner_term)) ** 2) ** (-0.2)
-    outer_term = 1 / (1 + 0.25 * np.exp(-mass_vector[1:] ** (0.33)))
-
-    attack_probability = outer_term * second_term
-
-    encounter_rate = search_rate * attack_probability * 0.3
-
-    # 28*0.2 * (mass_vector[1:])**(beta-1)
-
-    handling_time = h0 * mass_vector ** (-beta) #/seconds_per_month
-    ci = 3.5 * loss_rate * (mass_vector[1:] ** (beta)) *seconds_per_month
-    maximum_consumption_rate = 7 * loss_rate * (mass_vector[1:] ** (beta)) *seconds_per_month
-
-    nu = maximum_consumption_rate * 1/encounter_rate
-    print(nu, maximum_consumption_rate,  r0*mass_vector[0]**(beta), ci, encounter_rate)
-    return ci, nu, maximum_consumption_rate, r0*mass_vector[0]**(beta)
+    print(cp*C_star/(C_star+nu1), epsn * C_star*R_star*cmax/(R_star+nu0))
+    return np.array([R_star, C_star, P_star])
 
 
-parameter_calculator(mass_vector)
+mass_vector = np.array([1, 1, 100])
+
+def parameter_calculator_mass(mass_vector):
+    alpha = 15
+    b = 330/12
+    v = 0.2 #/12
+    maximum_consumption_rate = alpha * mass_vector[1:]**(0.75)
+
+    ci = v*maximum_consumption_rate
+    ci[0] = ci[0]
+    #ci[-1] = ci[-1]*0.1
+    #print(maximum_consumption_rate)
+    r0  = 0.1
+    nu = alpha/b*mass_vector[1:]**(0)
+    #print(ci)
+    return ci, nu, maximum_consumption_rate, r0
+
+
+cost_of_living, nu, growth_max, lam = parameter_calculator_mass(mass_vector)
+print(nu)
+base = 30 #3 #*mass_vector[0]**(-0.25) #0.01
+cbar = base
+phi0_base = cost_of_living[1]/2#*5
+phi1 = cost_of_living[1]/2 #*2#/2#*5
+phi0 = phi0_base
+eps = 0.7
+epsn = 0.7
+
+cmax, cp = growth_max
+mu0 = cost_of_living[0]/2 #*2#/2#*5 #*60 #/2
+mu1 = cost_of_living[0]/2 #*2 #*2#/2#*5 #*2 #*10 #/2
+nu0 = nu[0] #nu
+nu1 = nu[1] #nu
+
+params_ext = {'cmax' : cmax, 'mu0' : mu0, 'mu1' : mu1, 'eps': eps, 'epsn': epsn, 'cp': cp, 'phi0':phi0, 'phi1': phi1,
+          'resource': base, 'lam':lam, 'nu0':nu0, 'nu1': nu1}
+
+print(static_eq_calc(params_ext), base)